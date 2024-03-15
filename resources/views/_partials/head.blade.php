684494b8f3fd47fc8a6ed7d290dc8a431678b18f
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-03-15 16:40:42 +0100

    Update style project

--- resources/views/_partials/head.blade.php
+++ resources/views/_partials/head.blade.php
@@ -3,5 +3,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
     <title>A Web Dev Journey</title>
 </head>
