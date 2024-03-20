093102985897d517e24bb7ec0665aafa5e3c8fb7
PIeter <pieter_ophalvens@hotmail.com>
2024-03-20 16:34:04 +0100

    styled guessing and rps game

--- resources/views/guessing.blade.php
+++ resources/views/guessing.blade.php
@@ -1,27 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" media="screen" href="css/guessing.css" />
-        <title>A Web Dev Journey</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/guessing.css" />
+    <title>A Web Dev Journey</title>
+</head>
 <body>
     @include("_partials.header") 
     <main class="game">
         <div class="terminal">
+            <div class="popup terminal" id="lost" style="display: none">
+                <div class="bar">
+                    <p>📟 YOU LOSE 😭</p>
+                </div>
+                <div class="content">
+                    <a href="/story">> RESTART</a>
+                </div>
+            </div>
+            <div class="popup terminal" id="won" style="display: none">
+                <div class="bar">
+                    <p>📟 YOU WIN 😁</p>
+                </div>
+                <div class="content">
+                    <a href="/story">> NICE! CONTINUE</a>
+                </div>
+            </div>
             <div class="bar">
                 <p>📟 GUESSING GAME</p>
             </div>
             <div class="content">
-    @include("_partials.games.guessing")
+                @include("_partials.games.guessing")
             </div>
         </div>
     </main>
 </body>
-</html>
