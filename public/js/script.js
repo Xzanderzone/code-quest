4aad83f4b90b5d0c0abdb8ddec10f65e98e2a533
Gustave Curtil <gustave.curtil@tutanota.com>
2024-03-20 16:31:47 +0100

    dragable stuff is for real

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+var offset = [0,0];
+var divOverlay = document.getElementById("won");
+var isDown = false;
+divOverlay.addEventListener('mousedown', function(e) {
+    isDown = true;
+    offset = [
+        divOverlay.offsetLeft - e.clientX,
+        divOverlay.offsetTop - e.clientY
+    ];
+}, true);
+document.addEventListener('mouseup', function() {
+    isDown = false;
+}, true);
+
+document.addEventListener('mousemove', function(e) {
+    event.preventDefault();
+    if (isDown) {
+        divOverlay.style.left = (e.clientX + offset[0]) + 'px';
+        divOverlay.style.top  = (e.clientY + offset[1]) + 'px';
+    }
+}, true);
