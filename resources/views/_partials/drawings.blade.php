3ae63a9db24310d68e9b1b285fe79fb738ccf520
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-03-25 12:15:20 +0100

    Connect animations to images for character selection

--- resources/views/_partials/drawings.blade.php
+++ resources/views/_partials/drawings.blade.php
@@ -3,6 +3,9 @@
 @if(!empty($user->character))
     <div class="you" id="userContainer">
         <img id="userImage" src="..\media\stickmen\{{ $user->character }}" alt="">
-        <img class="gif" id="gifImage" src="..\media\animations\Stickman-codequest-glasses.gif" alt="">
+        @php
+            $gifFilename = pathinfo($user->character, PATHINFO_FILENAME) . '.gif';
+        @endphp
+        <img class="gif" id="gifImage" src="..\media\animations\{{ $gifFilename }}" alt="">
     </div>
 @endif
