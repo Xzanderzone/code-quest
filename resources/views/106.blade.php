ff652e78d3c02d9cbfb20333f71695e169c0f4f2
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-03-25 13:31:15 +0100

    Add id 'animationButton' in blades

--- resources/views/106.blade.php
+++ resources/views/106.blade.php
@@ -7,7 +7,7 @@
     <main class="star">
         <p>Congratz! You earned your first Softskill:</p>
         <h2>{{$user->card_reason}}</H2>
-            <a class="pixel" href="/next" id="animationButton"><p class="matrix-text">I'm happy!</p></a>
+            <a class="pixel" href="/next"><p class="matrix-text">I'm happy!</p></a>
     </main>
 </body>
 </html>
