d0aa1b9efc3a7ee36936b2f743c3ecbc177f3aac
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-03-27 10:29:58 +0100

    Change header in cv-blade

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -4,13 +4,16 @@
 <body>
     <header>
         <div id="logo"> 
-            <img src="..\media\logo\Logo.svg" alt="Code Guest Logo">
-        </div>
-        <div id="title">
-            <h1>
-                <a href="/" title="Go back to the landing page"> A WEB DEV JOURNEY</a>
-            </h1>
-        </div>
+        <a href="/" title="Go back to the landing page">
+        <img class="logoBlack" src="..\media\logo\Logo.svg" alt="Code Guest Logo">
+        <img class="logoWhite" src="..\media\logo\Logo-white.svg" alt="Code Guest Logo Hover">
+        </a>
+    </div>
+    <div id="title">
+        <h1>
+             A WEB DEV JOURNEY
+        </h1>
+    </div>
         <div id="cv-emblem">
             @auth
             <a href="/story">back to story</a>
