97e5605d3b950afab3e0e1a9870022e2943d2aae
Luis <luismaja.work@gmail.com>
2024-03-29 14:09:01 +0100

    add java icons and fix pathing to the 3 folders

--- public/js/memory/index.js
+++ public/js/memory/index.js
@@ -7,34 +7,42 @@ let score = 0;
 
 document.querySelector(".score").textContent = score;
 
-switch(course) {
-  case fullstack:
-    fetch("/media/memory_cards/fullstack/fullstack.json")
-    .then((res) => res.json())
-    .then((data) => {
-      cards = [...data, ...data];
-      shuffleCards();
-      generateCards();
-    });
-    break;
-  case salesforce:
-    fetch("/media/memory_cards/salesforce/salesforce.json")
-    .then((res) => res.json())
-    .then((data) => {
-      cards = [...data, ...data];
-      shuffleCards();
-      generateCards();
-    });
-    break;
-  case java:
-    fetch("/media/memory_cards/java/java.json")
-    .then((res) => res.json())
-    .then((data) => {
-      cards = [...data, ...data];
-      shuffleCards();
-      generateCards();
-    });
-}
+// switch(course) {
+//   case fullstack:
+//     fetch("/media/memory_cards/fullstack/fullstack.json")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       cards = [...data, ...data];
+//       shuffleCards();
+//       generateCards();
+//     });
+//     break;
+//   case salesforce:
+//     fetch("/media/memory_cards/salesforce/salesforce.json")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       cards = [...data, ...data];
+//       shuffleCards();
+//       generateCards();
+//     });
+//     break;
+//   case java:
+//     fetch("/media/memory_cards/java/java.json")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       cards = [...data, ...data];
+//       shuffleCards();
+//       generateCards();
+//     });
+// }
+
+fetch("/media/memory_cards/java/java.json")
+.then((res) => res.json())
+.then((data) => {
+  cards = [...data, ...data];
+  shuffleCards();
+  generateCards();
+});
 
 
 function shuffleCards() {
@@ -145,6 +153,3 @@ let firstPair = matchedPair[0];
 let fourthPair = matchedPair[3];
 let eighthPair = matchedPair[7];
 
-getCourseChoice() {
-  
-}
