b71273117e0d400cd6d4557ab9490622de025219
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-03-31 22:15:46 +0200

    Change sad face and transparent background

--- resources/views/204.blade.php
+++ resources/views/204.blade.php
@@ -7,38 +7,46 @@
         @csrf
             @if ($user->toilet_choice == "Initiative")
             <main class="star">
-                <p>Congratz! You unlocked a Softskill:</p>
-                <h2>{{$user->toilet_choice}}</h2>
-                <a class="pixel" href="/next"><p class="matrix-text">I'm happy!</p></a>
+                <div class="whiteBlock">
+                    <p>Congratz! You unlocked a Softskill:</p>
+                    <h2>{{$user->toilet_choice}}</h2>
+                    <a class="pixel" href="/next"><p class="matrix-text">I'm happy!</p></a>
+                </div>
             </main>
             @elseif ($user->toilet_choice == "Conflict Resolution")
             <main class="star">
-                <p>Congratz! You unlocked 2 Softskills:</p>
-                <h2>{{$user->toilet_choice}} and 
-                    @if ($user->seats === 'empty-table')
-                    Empathy
-                    @else
-                        Positive Attitude
-                    @endif
-                </h2>
-                <form action="/extra-toilet-skill" method="POST">
-                    @csrf
-                    <button class="pixel matrix-text">I'm happy!</button>
-                </form>
+                <div class="whiteBlock">
+                    <p>Congratz! You unlocked 2 Softskills:</p>
+                    <h2>{{$user->toilet_choice}} and 
+                        @if ($user->seats === 'empty-table')
+                        Empathy
+                        @else
+                            Positive Attitude
+                        @endif
+                    </h2>
+                    <form action="/extra-toilet-skill" method="POST">
+                        @csrf
+                        <button class="pixel matrix-text">I'm happy!</button>
+                    </form>
+                </div>
             </main>
             @elseif ($user->card_reason === "Leadership" || $user->card_reason === "Communication")
             <main class="sad">
-                <h2>Your colleagues are disappointed in you. {{$user->card_reason}} was removed from your CV.</h2>
-                <form action="/card-reason-update" method="POST">
-                    @csrf
-                    <button class="pixel matrix-text">Let's move on</button>
-                </form>
+                <div class="whiteBlock">
+                    <h2>Your colleagues are disappointed in you. {{$user->card_reason}} was removed from your CV.</h2>
+                    <form action="/card-reason-update" method="POST">
+                        @csrf
+                        <button class="pixel matrix-text">Let's move on</button>
+                    </form>
+                </div>
             </main>
             @elseif ($user->card_reason !== "Leadership" && $user->card_reason !== "Communication" && !is_null($user->card_reason))
             <main class="sad">
-                <h2>Your colleagues are starting to keep their distance from you and your CV progress is delayed, causing you to not acquire a new soft skill.</h2>
-                <a class="pixel" href="/next"><p class="matrix-text">Let's move on</p></a>
-            @endif
+                <div class="whiteBlock">
+                    <h2>Your colleagues are starting to keep their distance from you and your CV progress is delayed, causing you to not acquire a new soft skill.</h2>
+                    <a class="pixel" href="/next"><p class="matrix-text">Let's move on</p></a>
+                </div>
             </main>
+            @endif
 </body>
 </html>
