d6dea7e64d1d7057e6a6d0762ff74725ccd87f8e
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-04-01 13:40:58 +0200

    Add whiteBlock class in div to the first soft skill blade

--- resources/views/106.blade.php
+++ resources/views/106.blade.php
@@ -5,9 +5,11 @@
 <body>
     @include("_partials.header") 
     <main class="star">
+        <div class="whiteBlock">
         <p>Congratz! You earned your first Softskill:</p>
         <h2>{{$user->card_reason}}</H2>
-            <a class="pixel" href="/next"><p class="matrix-text">I'm happy!</p></a>
+        <a class="pixel" href="/next"><p class="matrix-text">I'm happy!</p></a>
+        </div>
     </main>
 </body>
 </html>
