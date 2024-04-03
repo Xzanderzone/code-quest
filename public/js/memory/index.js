8efffd2cd976fd68b95d69f968e89db9f563483e
PIeter <pieter_ophalvens@hotmail.com>
2024-04-03 15:44:44 +0200

    rework login Dump and Dies

--- public/js/memory/index.js
+++ public/js/memory/index.js
@@ -7,144 +7,148 @@ let score = 0;
 
 document.querySelector(".score").textContent = score;
 
-let course = document.currentScript.getAttribute("track")
+let course = document.currentScript.getAttribute("track");
 console.log(course);
 
-switch(course) {
-  case "Fullstack":
-    fetch("/media/memory_cards/fullstack/fullstack.json")
-    .then((res) => res.json())
-    .then((data) => {
-      cards = [...data, ...data];
-      shuffleCards();
-      generateCards();
-    });
-    break;
-  case "Salesforce":
-    fetch("/media/memory_cards/salesforce/salesforce.json")
-    .then((res) => res.json())
-    .then((data) => {
-      cards = [...data, ...data];
-      shuffleCards();
-      generateCards();
-    });
-    break;
-  case "Java":
-    fetch("/media/memory_cards/java/java.json")
-    .then((res) => res.json())
-    .then((data) => {
-      cards = [...data, ...data];
-      shuffleCards();
-      generateCards();
-    });
+switch (course) {
+    case "Fullstack":
+        fetch("/media/memory_cards/fullstack/fullstack.json")
+            .then((res) => res.json())
+            .then((data) => {
+                cards = [...data, ...data];
+                shuffleCards();
+                generateCards();
+            });
+        break;
+    case "Salesforce":
+        fetch("/media/memory_cards/salesforce/salesforce.json")
+            .then((res) => res.json())
+            .then((data) => {
+                cards = [...data, ...data];
+                shuffleCards();
+                generateCards();
+            });
+        break;
+    case "Java":
+        fetch("/media/memory_cards/java/java.json")
+            .then((res) => res.json())
+            .then((data) => {
+                cards = [...data, ...data];
+                shuffleCards();
+                generateCards();
+            });
 }
 
-
 function shuffleCards() {
-  let currentIndex = cards.length,
-    randomIndex,
-    temporaryValue;
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = cards[currentIndex];
-    cards[currentIndex] = cards[randomIndex];
-    cards[randomIndex] = temporaryValue;
-  }
+    let currentIndex = cards.length,
+        randomIndex,
+        temporaryValue;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = cards[currentIndex];
+        cards[currentIndex] = cards[randomIndex];
+        cards[randomIndex] = temporaryValue;
+    }
 }
 
 function generateCards() {
-  for (let card of cards) {
-    const cardElement = document.createElement("div");
-    cardElement.classList.add("card");
-    cardElement.setAttribute("data-name", card.name);
-    cardElement.innerHTML = `
+    for (let card of cards) {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+        cardElement.setAttribute("data-name", card.name);
+        cardElement.innerHTML = `
       <div class="front">
         <img class="front-image" src=${card.image} />
       </div>
       <div class="back"></div>
     `;
-    gridContainer.appendChild(cardElement);
-    cardElement.addEventListener("click", flipCard);
-  }
+        gridContainer.appendChild(cardElement);
+        cardElement.addEventListener("click", flipCard);
+    }
 }
 
 function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
-  this.classList.add("flipped");
+    this.classList.add("flipped");
 
-  if (!firstCard) {
-    firstCard = this;
-    return;
-  }
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
 
-  secondCard = this;
-  score++;
-  document.querySelector(".score").textContent = score;
-  lockBoard = true;
+    secondCard = this;
+    score++;
+    document.querySelector(".score").textContent = score;
+    lockBoard = true;
 
-  checkForMatch();
+    checkForMatch();
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
 
-  if (isMatch) {
-    if (!isPairMatched(firstCard.dataset.name, secondCard.dataset.name)) {
-      addMatchedPair(firstCard.dataset.name, secondCard.dataset.name);
-      console.log("Pair matched:", firstCard.dataset.name, secondCard.dataset.name);
+    if (isMatch) {
+        if (!isPairMatched(firstCard.dataset.name, secondCard.dataset.name)) {
+            addMatchedPair(firstCard.dataset.name, secondCard.dataset.name);
+            console.log(
+                "Pair matched:",
+                firstCard.dataset.name,
+                secondCard.dataset.name
+            );
+        } else {
+            console.log("Pair already matched!");
+        }
+        disableCards();
     } else {
-      console.log("Pair already matched!");
+        unflipCards();
     }
-    disableCards();
-  } else {
-    unflipCards();
-  }
 
-  console.log(matchedPair)
+    console.log(matchedPair);
 }
 
 function disableCards() {
-  firstCard.removeEventListener("click", flipCard);
-  secondCard.removeEventListener("click", flipCard);
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
 
-  resetBoard();
+    resetBoard();
 }
 
 function unflipCards() {
-  setTimeout(() => {
-    firstCard.classList.remove("flipped");
-    secondCard.classList.remove("flipped");
-    resetBoard();
-  }, 1000);
+    setTimeout(() => {
+        firstCard.classList.remove("flipped");
+        secondCard.classList.remove("flipped");
+        resetBoard();
+    }, 1000);
 }
 
 function resetBoard() {
-  firstCard = null;
-  secondCard = null;
-  lockBoard = false;
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
 }
 
 function restart() {
-  resetBoard();
-  shuffleCards();
-  score = 0;
-  document.querySelector(".score").textContent = score;
-  gridContainer.innerHTML = "";
-  generateCards();
+    resetBoard();
+    shuffleCards();
+    score = 0;
+    document.querySelector(".score").textContent = score;
+    gridContainer.innerHTML = "";
+    generateCards();
 }
 
 function isPairMatched(firstCardName, secondCardName) {
-  return matchedPair.some(pair => pair.includes(firstCardName) && pair.includes(secondCardName));
+    return matchedPair.some(
+        (pair) => pair.includes(firstCardName) && pair.includes(secondCardName)
+    );
 }
 
 function addMatchedPair(firstCardName, secondCardName) {
-  matchedPair.push([firstCardName, secondCardName]);
+    matchedPair.push([firstCardName, secondCardName]);
 }
 
 let firstPair = matchedPair[0];
 let fourthPair = matchedPair[3];
 let eighthPair = matchedPair[7];
-
