8f5a1bc6d7ba456d453eeb41a1a7386ddf5c572b
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-04-04 13:48:25 +0200

    Add animations waving at the ending

---
+++ resources/views/_partials/end-footer.blade.php
@@ -0,0 +1,7 @@
+ <div class="end-footer">
+    <img src="..\media\stickmen\Bonasi-crying.gif">
+    <img src="..\media\stickmen\Stickman-persile-v1.gif">
+    <img src="..\media\stickmen\Stickman-erwin.gif">
+    <img src="..\media\stickmen\saleforce.gif">
+    <img src="..\media\stickmen\java.gif">
+</div>
