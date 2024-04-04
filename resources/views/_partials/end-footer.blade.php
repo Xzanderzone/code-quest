931bcd1753aa4e17d73076e5a8e06add11c59bf4
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-04-04 14:19:26 +0200

    Change footer ending with animations

--- resources/views/_partials/end-footer.blade.php
+++ resources/views/_partials/end-footer.blade.php
@@ -4,4 +4,10 @@
     <img src="..\media\stickmen\Stickman-erwin.gif">
     <img src="..\media\stickmen\saleforce.gif">
     <img src="..\media\stickmen\java.gif">
+    <img src="..\media\stickman-ending\Stickman-ending-glasses.gif">
+    <img src="..\media\stickman-ending\Stickman-ending-hair.gif">
+    <img src="..\media\stickman-ending\Stickman-ending-hat.gif">
+    <img src="..\media\stickman-ending\Stickman-ending-wheelchair.gif">
+    <img src="..\media\stickman-ending\Stickman-ending-partyhat.gif">
+    <img src="..\media\stickman-ending\Stickman-ending-moustache.gif">
 </div>
