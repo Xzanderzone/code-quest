c7d905d498345268f1535d3f22ef71cf35d2e12b
Luis <luismaja.work@gmail.com>
2024-04-08 11:33:50 +0200

    fix names to match json

--- resources/views/memory.blade.php
+++ resources/views/memory.blade.php
@@ -19,7 +19,7 @@
             <div class="bar">
                 <p>📟 MEMORY GAME</p>
                 <div class="barBtns">
-                <button id='skipBar' title="Unlocks in 10 turns" disabled>Skip</button>
+                <button id='skip' title="Unlocks in 10 turns" disabled>Skip</button>
 		        <button id='infoBtn'>Help</button>
                 </div>
             </div>
